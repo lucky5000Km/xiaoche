e0e4f1f811fdbe19d86421261205a4fa36a8b42c
lichaobao <1527563274@qq.com>
2022-01-09 12:31:09 +0800

    设置测试版appId

--- miniprogram/envList.js
+++ miniprogram/envList.js
@@ -1,4 +1,4 @@
-const envList = [{"envId":"cloud1-5g7a4pr71dc70177","alias":"cloud1"}]
+const envList = [{"envId":"cloud1-8gp4og4416301c85","alias":"cloud1"}]
 const isMac = false
 module.exports = {
     envList,
